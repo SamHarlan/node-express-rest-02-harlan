26f95cf2865c18a85563e69e4dc28e442772452c
Sam Harlan <sams.email24@gmail.com>
2019-05-24 11:37:23 -0700

    edit

--- friends-router-01.js
+++ friends-router-01.js
@@ -5,14 +5,14 @@ var friendModel = require('./models/friends-01');
 
 
 
-router.post('/friends', 
+router.post('/friends',
     function FriendsPostHandler(request, response){
-        friendModel.insert( 
+        friendModel.insert(
             request.body.birthDate,
             request.body.firstName,
             request.body.lastName,
             request.body.gender,
-            request.body.phone, 
+            request.body.phone,
             function DoneInserting(err, resultId){
                 if (err){
                     console.log("Some error inserting");
@@ -24,7 +24,7 @@ router.post('/friends',
             } );
 });
 
-router.get('/friends', function FriendsGetHandler(request, response){
+router.get('/all', function FriendsGetHandler(request, response){
     friendModel.getAll(function DoneGettingAll(err, result, fields){
         if (err) {
             console.log("Some error selecting all");
